2e656732ddae6cbbf6df3905c6d04c17068ec041
Shakhawat Hossain <shakhawat.sk@gmail.com>
2020-05-20 17:15:03 +0600

    Install Laravel passport

--- app/User.php
+++ app/User.php
@@ -5,11 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 use App\Role;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use HasApiTokens, Notifiable;
 
     /**
      * The attributes that are mass assignable.
--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.fontend')
 
 @section('content')
 <div class="container">
